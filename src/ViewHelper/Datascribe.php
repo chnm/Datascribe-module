03851fccde14267259c1fd589b8d2f57fb104176
Jim Safley <jimsafley@gmail.com>
2019-10-15 21:17:12 -0400

    Minor approvements

--- src/ViewHelper/Datascribe.php
+++ src/ViewHelper/Datascribe.php
@@ -54,14 +54,22 @@ class Datascribe extends AbstractHelper
         return sprintf('<div class="breadcrumbs">%s</div>', implode('<div class="separator"></div>', $bc));
     }
 
+    /**
+     * Get the item review status select markup.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     public function itemReviewStatusSelect($name, $value, $label)
     {
         $view = $this->getView();
         $valueOptions = [
             'new' => $view->translate('New'),
             'in_progress' => $view->translate('In progress'),
-            'submitted_and_need_review' => $view->translate('Submitted and need review'),
-            'submitted_and_not_approved' => $view->translate('Submitted and not approved'),
+            'need_review' => $view->translate('Need review'),
+            'not_approved' => $view->translate('Not approved'),
             'approved' => $view->translate('Approved'),
         ];
         $select = (new Select($name))
@@ -72,6 +80,14 @@ class Datascribe extends AbstractHelper
         return $view->formRow($select);
     }
 
+    /**
+     * Get the item locked status select markup.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     public function itemLockedStatusSelect($name, $value, $label)
     {
         $view = $this->getView();
@@ -87,41 +103,83 @@ class Datascribe extends AbstractHelper
         return $view->formRow($select);
     }
 
+    /**
+     * Get the item locked by select markup.
+     *
+     * @param int $projectId
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     public function itemLockedBySelect($projectId, $name, $value, $label)
     {
         return $this->itemUserSelect('lockedBy', $projectId, $name, $value, $label);
     }
 
+    /**
+     * Get the item submitted by select markup.
+     *
+     * @param int $projectId
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     public function itemSubmittedBySelect($projectId, $name, $value, $label)
     {
         return $this->itemUserSelect('submittedBy', $projectId, $name, $value, $label);
     }
 
+    /**
+     * Get the item reviewed by select markup.
+     *
+     * @param int $projectId
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     public function itemReviewedBySelect($projectId, $name, $value, $label)
     {
         return $this->itemUserSelect('reviewedBy', $projectId, $name, $value, $label);
     }
 
+    /**
+     * Get the item user select markup.
+     *
+     * This will only get users who are set in the $byColumn.
+     *
+     * @param string $byColumn
+     * @param int $projectId
+     * @param string $name
+     * @param string $value
+     * @param string $label
+     * @return string
+     */
     protected function itemUserSelect($byColumn, $projectId, $name, $value, $label)
     {
         $view = $this->getView();
-        $em = $this->services->get('Omeka\EntityManager');
-        $dql = "
-            SELECT u
-            FROM Omeka\Entity\User u
-            JOIN Datascribe\Entity\DatascribeItem i WITH i.$byColumn = u
-            JOIN i.dataset d
-            JOIN d.project p
-            WHERE p = :projectId";
-        $query = $em->createQuery($dql);
-        $query->setParameter('projectId', $projectId);
-        $users = $query->getResult();
-        usort($users, function ($userA, $userB) {
-            return strcmp($userA->getName(), $userB->getName());
-        });
         $valueOptions = [];
-        foreach ($users as $user) {
-            $valueOptions[$user->getId()] = sprintf('%s (%s)', $user->getName(), $user->getEmail());
+        // Only query valid user columns.
+        if (in_array($byColumn, ['lockedBy', 'submittedBy', 'reviewedBy'])) {
+            $em = $this->services->get('Omeka\EntityManager');
+            $dql = "
+                SELECT u
+                FROM Omeka\Entity\User u
+                JOIN Datascribe\Entity\DatascribeItem i WITH i.$byColumn = u
+                JOIN i.dataset d
+                JOIN d.project p
+                WHERE p = :projectId";
+            $query = $em->createQuery($dql);
+            $query->setParameter('projectId', $projectId);
+            $users = $query->getResult();
+            usort($users, function ($userA, $userB) {
+                return strcmp($userA->getName(), $userB->getName());
+            });
+            foreach ($users as $user) {
+                $valueOptions[$user->getId()] = sprintf('%s (%s)', $user->getName(), $user->getEmail());
+            }
         }
         $select = (new Select($name))
             ->setLabel($label)
