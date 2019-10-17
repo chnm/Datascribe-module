8adf3448d4d8a89461d8ca2fa89b6c6e70dc09da
Jim Safley <jimsafley@gmail.com>
2019-10-17 16:01:03 -0400

    Begin implementing batch item edit form

--- src/Form/ItemBatchForm.php
+++ src/Form/ItemBatchForm.php
@@ -7,6 +7,64 @@ class ItemBatchForm extends Form
 {
     public function init()
     {
+        $this->add([
+            'type' => 'radio',
+            'name' => 'locked',
+            'options' => [
+                'label' => 'Lock  status', // @translate
+                'value_options' => [
+                    '1' => 'Locked', // @translate
+                    '0' => 'Unlocked', // @translate
+                    '' => '[No change]', // @translate
+                ],
+            ],
+            'attributes' => [
+                'value' => '',
+            ],
+        ]);
+        $this->add([
+            'type' => 'Omeka\Form\Element\UserSelect',
+            'name' => 'locked_user',
+            'options' => [
+                'label' => 'Lock to user', // @translate
+                'empty_option' => '',
+            ],
+            'attributes' => [
+                'class' => 'chosen-select',
+                'data-placeholder' => 'Select a user', // @translate
+            ],
+        ]);
+        $this->add([
+            'name' => 'prioritized',
+            'type' => 'radio',
+            'options' => [
+                'label' => 'Priority status', // @translate
+                'value_options' => [
+                    '1' => 'Prioritized', // @translate
+                    '0' => 'Not prioritized', // @translate
+                    '' => '[No change]', // @translate
+                ],
+            ],
+            'attributes' => [
+                'value' => '',
+            ],
+        ]);
+        $this->add([
+            'name' => 'reviewed',
+            'type' => 'radio',
+            'options' => [
+                'label' => 'Review status', // @translate
+                'value_options' => [
+                    '1' => 'Approved', // @translate
+                    '0' => 'Not approved', // @translate
+                    '' => '[No change]', // @translate
+                ],
+            ],
+            'attributes' => [
+                'value' => '',
+            ],
+        ]);
+
         $inputFilter = $this->getInputFilter();
     }
 }
