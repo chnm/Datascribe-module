6d084f921a4dfe16447b5a83e02a3f1d668fe3b2
Jim Safley <jimsafley@gmail.com>
2019-10-26 13:22:32 -0400

    Minor refactor

--- src/Api/Adapter/DatascribeDatasetAdapter.php
+++ src/Api/Adapter/DatascribeDatasetAdapter.php
@@ -116,8 +116,16 @@ class DatascribeDatasetAdapter extends AbstractEntityAdapter
                 // This field is in an invalid format. Ignore it.
                 continue;
             }
-            $field->setLabel($fieldFormData['o-module-datascribe:label'] ?? null);
-            $field->setHint($fieldFormData['o-module-datascribe:hint'] ?? null);
+
+            $label =
+                (isset($fieldFormData['o-module-datascribe:label']) && preg_match('/^.+$/', $fieldFormData['o-module-datascribe:label']))
+                ? $fieldFormData['o-module-datascribe:label'] : null;
+            $hint =
+                (isset($fieldFormData['o-module-datascribe:hint']) && preg_match('/^.+$/', $fieldFormData['o-module-datascribe:hint']))
+                ? $fieldFormData['o-module-datascribe:hint'] : null;
+
+            $field->setLabel($label);
+            $field->setHint($hint);
             $field->setIsPrimary($fieldFormData['o-module-datascribe:is_primary'] ?? false);
             $field->setPosition($position++);
             $dataType = $dataTypes->get($field->getDataType());
