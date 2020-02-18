6ba5406278fc4c9a5fd06c26c0df32252f3bb958
Jim Safley <jimsafley@gmail.com>
2020-02-18 13:27:34 -0500

    Bind left/right arrows to tablesaw left/right buttons

---
+++ asset/js/admin/record-browse.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+window.addEventListener('keydown', function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+    switch (event.key) {
+        case 'Left':
+        case 'ArrowLeft':
+            $('a.btn.left').click();
+            break;
+        case 'Right':
+        case 'ArrowRight':
+            $('a.btn.right').click();
+            break;
+        default:
+            return;
+    }
+    event.preventDefault();
+}, true);
+
+});
