3541327be04daac8c463c9c5d3acfafd0cd5bc21
Jim Safley <jimsafley@gmail.com>
2020-02-19 21:05:15 -0500

    Unknown data types should preserve field data and value data

--- src/DatascribeDataType/Fallback.php
+++ src/DatascribeDataType/Fallback.php
@@ -7,6 +7,11 @@ use Zend\InputFilter\InputFilter;
 
 class Fallback implements DataTypeInterface
 {
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
     public function getLabel() : string
     {
         return '[Unknown]'; // @translate
@@ -14,20 +19,65 @@ class Fallback implements DataTypeInterface
 
     public function addFieldDataElements(Fieldset $fieldset, array $fieldData) : void
     {
+        $element = new Element\Text('data_type');
+        $element->setLabel('Unknown data type'); // @translate
+        $element->setValue($this->name);
+        $element->setAttribute('disabled', true);
+        $fieldset->add($element);
+
+        $element = new Element\Textarea('field_data_disabled');
+        $element->setLabel('Field data'); // @translate
+        $element->setValue(json_encode($fieldData, JSON_PRETTY_PRINT));
+        $element->setAttributes([
+            'disabled' => true,
+            'rows' => 8,
+        ]);
+        $fieldset->add($element);
+
+        $element = new Element\Hidden('field_data');
+        $element->setValue(json_encode($fieldData));
+        $fieldset->add($element);
     }
 
     public function getFieldData(array $fieldFormData) : array
     {
-        return [];
+        $fieldData = $fieldFormData['field_data'] ?? null;
+        $fieldData = json_decode($fieldData, true);
+        return is_array($fieldData) ? $fieldData : [];
+    }
+
+    public function fieldDataIsValid(array $fieldData) : bool
+    {
+        return true;
     }
 
     public function addValueDataElements(Fieldset $fieldset, string $fieldName, ?string $fieldDescription, array $fieldData, array $valueData) : void
     {
+        $element = new Element\Text('data_type');
+        $element->setLabel('Unknown data type'); // @translate
+        $element->setValue($this->name);
+        $element->setAttribute('disabled', true);
+        $fieldset->add($element);
+
+        $element = new Element\Textarea('value_data_disabled');
+        $element->setLabel('Value data'); // @translate
+        $element->setValue(json_encode($valueData, JSON_PRETTY_PRINT));
+        $element->setAttributes([
+            'disabled' => true,
+            'rows' => 8,
+        ]);
+        $fieldset->add($element);
+
+        $element = new Element\Hidden('value_data');
+        $element->setValue(json_encode($valueData));
+        $fieldset->add($element);
     }
 
     public function getValueData(array $valueFormData) : array
     {
-        return [];
+        $valueData = $valueFormData['value_data'] ?? null;
+        $valueData = json_decode($valueData, true);
+        return is_array($valueData) ? $valueData : [];
     }
 
     public function valueDataIsValid(array $fieldData, array $valueData) : bool
