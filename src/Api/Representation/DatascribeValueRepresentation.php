8078577649aef3df92bcb49987f9ea80a531153c
Jim Safley <jimsafley@gmail.com>
2020-02-19 21:03:01 -0500

    Account for unknown data type when getting value

--- src/Api/Representation/DatascribeValueRepresentation.php
+++ src/Api/Representation/DatascribeValueRepresentation.php
@@ -1,6 +1,7 @@
 <?php
 namespace Datascribe\Api\Representation;
 
+use Datascribe\DatascribeDataType\Fallback;
 use Datascribe\Entity\DatascribeValue;
 use Omeka\Api\Representation\AbstractRepresentation;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -73,12 +74,26 @@ class DatascribeValueRepresentation extends AbstractRepresentation
         return $dataType->valueDataIsValid($field->data(), $this->data());
     }
 
+    /**
+     * Return this value.
+     *
+     * The options are:
+     * - length: the maximum length of the value (default is null)
+     * - trim_marker: a string that follows a value that exceeds the maximum length (defualt is null)
+     * - if_invalid_return: return this if the value is invalid (default is false)
+     * - if_unknown_return: return this if the value is unknown (default is false)
+     * - if_empty_return: return this if the value is empty (default is null)
+     *
+     * @param array $options
+     * @return mixed
+     */
     public function value(array $options = [])
     {
         // Set default options.
         $options['length'] = $options['length'] ?? null;
         $options['trim_marker'] = $options['trim_marker'] ?? null;
         $options['if_invalid_return'] = $options['if_invalid_return'] ?? false;
+        $options['if_unknown_return'] = $options['if_unknown_return'] ?? false;
         $options['if_empty_return'] = $options['if_empty_return'] ?? null;
 
         if (!$this->valueIsValid()) {
@@ -86,6 +101,9 @@ class DatascribeValueRepresentation extends AbstractRepresentation
         }
         $manager = $this->getServiceLocator()->get('Datascribe\DataTypeManager');
         $dataType = $manager->get($this->field()->dataType());
+        if ($dataType instanceof Fallback) {
+            return $options['if_unknown_return'];
+        }
         $value = $dataType->getValue($this->data());
         $valueLength = mb_strlen($value);
         if (0 === $valueLength) {
