3010139865a389be3e96565bd1eeaee1078e33d6
Jim Safley <jimsafley@gmail.com>
2020-03-12 09:14:42 -0400

    Minor refactor

--- src/Job/ValidateDataset.php
+++ src/Job/ValidateDataset.php
@@ -13,19 +13,24 @@ class ValidateDataset extends AbstractJob
      */
     public function perform()
     {
-        if (!is_numeric($this->getArg('datascribe_dataset_id'))) {
-            throw new Exception\RuntimeException('Missing datascribe_dataset_id');
-        }
         $services = $this->getServiceLocator();
         $em = $services->get('Omeka\EntityManager');
-        $dataset = $em->find(DatascribeDataset::class, $this->getArg('datascribe_dataset_id'));
+        $conn = $services->get('Omeka\Connection');
+        $dataTypes = $services->get('Datascribe\DataTypeManager');
+
+        $datasetId = $this->getArg('datascribe_dataset_id');
+        if (!is_numeric($datasetId)) {
+            throw new Exception\RuntimeException('Missing datascribe_dataset_id');
+        }
+        $dataset = $em->find(DatascribeDataset::class, $datasetId);
         if (null === $dataset) {
             throw new Exception\RuntimeException('Cannot find dataset');
         }
 
-        // When a user adds a field to a dataset, any records existing before
-        // that time will have uncreated values for that field. Here we create
-        // those uncreated values so they can be validated.
+        // Null-fill uncreated values of this dataset. When an admin adds a
+        // field to a dataset, any records existing before that time will have
+        // uncreated values for that field. Here we create those uncreated
+        // values and set the texts to null so they can be validated.
         $sql = '
             SELECT field.id AS field_id, record.id AS record_id
             FROM datascribe_dataset dataset
@@ -36,7 +41,6 @@ class ValidateDataset extends AbstractJob
             WHERE dataset.id = :datasetId
             AND value.id IS NULL
             LIMIT :limit';
-        $conn = $services->get('Omeka\Connection');
         $stmt = $conn->prepare($sql);
         $stmt->bindValue('datasetId', $dataset->getId(), \PDO::PARAM_INT);
         $stmt->bindValue('limit', 100, \PDO::PARAM_INT);
@@ -60,8 +64,9 @@ class ValidateDataset extends AbstractJob
             $em->clear();
         } while ($results);
 
-        // Here we validate this dataset against the rules currently set in the
-        // form builder. Once validated, we mark invalid values as invalid.
+        // Validate all values of this dataset. Here we validate this dataset
+        // against the rules currently set in the form builder. Once validated,
+        // we mark invalid values as invalid.
         $maxResults = 100;
         $offset = 0;
         $dql = '
@@ -74,7 +79,6 @@ class ValidateDataset extends AbstractJob
         $query = $em->createQuery($dql)
             ->setParameter('datasetId', $dataset->getId())
             ->setMaxResults($maxResults);
-        $dataTypes = $services->get('Datascribe\DataTypeManager');
         do {
             $result = $query->setFirstResult($offset)->getResult();
             foreach ($result as $value) {
