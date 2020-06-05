9decfc7b506bdc2ac318c3766ece856801336fec
Kim Nguyen <design@kimisgold.com>
2020-06-05 09:11:30 -0400

    Style common elements and set to null.

--- asset/js/admin/record-form.js
+++ asset/js/admin/record-form.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 $('.set-null-button').on('click', function(e) {
     var thisButton = $(this);
-    var setNullCheckbox = thisButton.closest('fieldset').find('.common-elements .set-null-input');
+    var setNullCheckbox = thisButton.closest('fieldset').toggleClass('set-to-null').find('.common-elements .set-null-input');
     var setNullValue = ('0' === setNullCheckbox.val()) ? '1' : '0';
     setNullCheckbox.val(setNullValue);
 });
