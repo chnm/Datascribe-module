9ee2d414bbf360c3b1812673fbf6dcbd55fb5176
Kim Nguyen <design@kimisgold.com>
2020-06-12 16:46:31 -0400

    Add previous/next records toggle. (#39)

--- asset/js/admin/record-form.js
+++ asset/js/admin/record-form.js
@@ -5,4 +5,9 @@ $('.set-null-checkbox input[type="checkbox"]').on('change', function(e) {
     var setNullCheckbox = thisCheckbox.closest('fieldset').toggleClass('set-to-null');
 });
 
+$('.previous-next-toggle').click(function() {
+    var container = $(this).parents('.tablesaw-swipe-wrapper');
+    container.toggleClass('collapsed');  
+});
+
 });
