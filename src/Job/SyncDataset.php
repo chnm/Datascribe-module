d9ec9de585524373ce1fea7369dda215bc5dca8e
Jim Safley <jimsafley@gmail.com>
2021-02-04 14:52:05 -0500

    Chunk entity creation to flush every 100 times

--- src/Job/SyncDataset.php
+++ src/Job/SyncDataset.php
@@ -39,13 +39,16 @@ class SyncDataset extends AbstractJob
         $toCreate = array_diff($oItems, $dsItems);
 
         // Create new DataScribe items.
-        foreach ($toCreate as $oItemId) {
-            $dsItem = new DatascribeItem;
-            $dsItem->setDataset($dataset);
-            $dsItem->setItem($em->getReference(Item::class, $oItemId));
-            $dsItem->setSynced(new DateTime('now'));
-            $dsItem->setSyncedBy($this->job->getOwner());
-            $em->persist($dsItem);
+        foreach (array_chunk($toCreate, 100) as $toCreateChunk) {
+            foreach ($toCreateChunk as $oItemId) {
+                $dsItem = new DatascribeItem;
+                $dsItem->setDataset($dataset);
+                $dsItem->setItem($em->getReference(Item::class, $oItemId));
+                $dsItem->setSynced(new DateTime('now'));
+                $dsItem->setSyncedBy($this->job->getOwner());
+                $em->persist($dsItem);
+            }
+            $em->flush();
         }
 
         // Delete removed DataScribe items.
