0357d7f0d03df1950cec3c9f82e316cc2f6edfee
Jim Safley <jimsafley@gmail.com>
2021-02-25 16:14:35 -0500

    Remove arrow key navigation used for record tables
    The script was too greedy and captured all arrow key presses in the
    window. This prevented the use of arrow keys anywhere else in the
    interface. For now I'm removing key navigation in favor of button
    navigation until we come up with another solution. (fix #80)

--- asset/js/datascribe.js
+++ asset/js/datascribe.js
@@ -1,22 +1,2 @@
-var Datascribe = {
-    enableTableNavArrows: function() {
-        window.addEventListener('keydown', function (event) {
-            if (event.defaultPrevented) {
-                return;
-            }
-            switch (event.key) {
-                case 'Left':
-                case 'ArrowLeft':
-                    $('a.btn.left').click();
-                    break;
-                case 'Right':
-                case 'ArrowRight':
-                    $('a.btn.right').click();
-                    break;
-                default:
-                    return;
-            }
-            event.preventDefault();
-        }, true);
-    }
-};
+// DataScribe object for general use across all pages. Loaded in Module::appendToLayout().
+var Datascribe = {};
